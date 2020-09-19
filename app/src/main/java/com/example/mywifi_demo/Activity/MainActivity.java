fb57dc6554808609030ab9540fb5573dff436e6a
sun0-j <52477333+sun0-j@users.noreply.github.com>
2020-09-19 22:10:20 +0900

    디자인(아이콘,인트로,배경색,style.xml) 변경

--- app/src/main/java/com/example/mywifi_demo/Activity/MainActivity.java
+++ app/src/main/java/com/example/mywifi_demo/Activity/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.CheckBox;
