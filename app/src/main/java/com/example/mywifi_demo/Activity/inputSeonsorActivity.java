1c7d531d50fd533447bb4fdf5f212e6c89e24497
sun0-j <52477333+sun0-j@users.noreply.github.com>
2020-10-11 20:44:02 +0900

    센서 입력프레그먼트 추가

--- app/src/main/java/com/example/mywifi_demo/Activity/inputSeonsorActivity.java
+++ app/src/main/java/com/example/mywifi_demo/Activity/inputSeonsorActivity.java
@@ -3,6 +3,12 @@ package com.example.mywifi_demo.Activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.example.mywifi_demo.R;
 
@@ -12,5 +18,24 @@ public class inputSeonsorActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input_seonsor);
+        Button btn_connect = (Button) findViewById(R.id.btn_connect);
+        Spinner spinner = (Spinner)findViewById(R.id.spin_sensor);
+        final TextView txt_sensor=(TextView)findViewById(R.id.txt_sensor);
+        TextView txt_sensing=(TextView)findViewById(R.id.txt_sensing);
+
+        // Spinner
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                txt_sensor.setText("입력할 센서 : " + parent.getItemAtPosition(position));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
     }
+
 }
